e59e6c18f44cbd4acc228b6302ca50128e37b5c1
Afnan Navaz <afnannavaz@gmail.com>
2019-01-22 18:23:14 +0530

    nav bar

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function para() {
+    if(window.scrollY == 0) {
+        document.getElementById('navbr').style.backgroundColor="transparent";
+        document.getElementById('navbr').style.color="black"
+        }
+    else{
+        document.getElementById('navbr').style.backgroundColor="#202020";
+        document.getElementById('navbr').style.color="white"
+        }
+    }
