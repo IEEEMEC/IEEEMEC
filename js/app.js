10d20125c9645e32ea33870742768cd4cb8132b3
Afnan Navaz <afnannavaz@gmail.com>
2019-01-22 20:49:30 +0530

    scroll

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
-function para() {
-    if(window.scrollY == 0) {
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+  if(window.scrollY == 0) {
         document.getElementById('navbr').style.backgroundColor="transparent";
         document.getElementById('navbr').style.color="black"
         }
@@ -7,4 +13,4 @@ function para() {
         document.getElementById('navbr').style.backgroundColor="#202020";
         document.getElementById('navbr').style.color="white"
         }
-    }
+}
