3e7d296d4b13968aefa488169c261423432015f3
Devdutt Shenoi <devdutt@outlook.in>
2019-02-15 10:43:15 +0530

    Sets navbar

--- js/app.js
+++ js/app.js
@@ -7,10 +7,12 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
   if(window.scrollY == 0) {
         document.getElementById('navbar').style.backgroundColor="transparent";
-        document.getElementById('navbar').style.color="white"
+        document.getElementById('navbar').style.color="white";
+        document.getElementById('navbar').boxShadow="";
         }
     else{
-        document.getElementById('navbar').style.backgroundColor="rgba( 0, 0, 0, 0.85)";
-        document.getElementById('navbar').style.color="white"
+        document.getElementById('navbar').style.backgroundColor="rgba(250, 250, 250, 0.40)";
+        document.getElementById('navbar').style.color="#020202";
+        document.getElementById('navbar').boxShadow="-21.213px 21.213px 30px 0px rgba(158, 158, 158, 0.3";
         }
 }
