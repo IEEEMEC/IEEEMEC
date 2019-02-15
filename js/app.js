49ab413c27cad4845ca396c239978e0af5ce88da
Devdutt Shenoi <devdutt@outlook.in>
2019-02-15 12:52:07 +0530

    Minor Corrections

--- js/app.js
+++ js/app.js
@@ -7,12 +7,10 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
   if(window.scrollY == 0) {
         document.getElementById('navbar').style.backgroundColor="transparent";
-        document.getElementById('navbar').style.color="white";
         document.getElementById('navbar').boxShadow="";
         }
     else{
-        document.getElementById('navbar').style.backgroundColor="rgba(250, 250, 250, 0.40)";
-        document.getElementById('navbar').style.color="#020202";
+        document.getElementById('navbar').style.backgroundColor="rgba(25, 25, 25, 0.70)";
         document.getElementById('navbar').boxShadow="-21.213px 21.213px 30px 0px rgba(158, 158, 158, 0.3";
         }
 }
