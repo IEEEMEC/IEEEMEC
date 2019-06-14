a9a716d218ef3de73b14f315cc0dd86fc05a36bd
Devdutt Shenoi <devdutt@outlook.in>
2019-06-14 23:48:57 +0530

    Update from main

--- js/app.js
+++ js/app.js
@@ -1,16 +1,57 @@
-window.onscroll = function() {myFunction()};
+/* Loader */
+window.onload = function () {
+  document.getElementById("loader").classList.add("se-pre-con");
+}
+
+window.onscroll = function () { myFunction() };
+
+// Get the header
+let header = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+let sticky = header.offsetTop;
 
 function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
-  if(window.scrollY == 0) {
-        document.getElementById('navbar').style.backgroundColor="transparent";
-        document.getElementById('navbar').boxShadow="";
-        }
-    else{
-        document.getElementById('navbar').style.backgroundColor="rgba(25, 25, 25, 0.70)";
-        document.getElementById('navbar').boxShadow="-21.213px 21.213px 30px 0px rgba(158, 158, 158, 0.3";
-        }
+
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    header.classList.remove("static");
+  } else {
+    header.classList.add("static");
+    header.classList.remove("sticky");
+  }
 }
+
+
+function hamBurger() {
+  let x = document.getElementById("navbar");
+  if (x.classList.contains("visible")) {
+    x.classList.remove("visible");
+  } else {
+    x.classList.add("visible");
+  }
+}
+/*
+function carousel() {
+  let a = window.getComputedStyle(document.getElementById("one"));
+  let b = window.getComputedStyle(document.getElementById("two"));
+  let c = window.getComputedStyle(document.getElementById("three"));
+  if (a.display == "block") {
+    a.display = "none";
+    b.display = "block";
+  } else if (b.display == "block") {
+    b.display = "none";
+    c.display = "block";
+  } else {
+    c.display = "none";
+    a.display = "block";
+  }
+}
+
+let myCar = setInterval(carousel, 1000);
+*/
